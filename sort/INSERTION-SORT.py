60a84f721486ef6203a1845dc85de5bd577c9854
SUN Hao <sunhaohit0@gmail.com>
2015-05-22 15:28:50 +0800

    算法：插入排序

---
+++ sort/INSERTION-SORT.py
@@ -0,0 +1,24 @@
+##!/usr/bin/env python
+
+def INSERTION_SORT(A):
+        "INSERTION_SORT"
+        for j in range(1, len(A)-1):
+                key = A[j]
+                i = j-1
+                while i>=0 and A[i]>key:
+                        A[i+1] = A[i]
+                        i-=1
+                A[i+1]=key
+        return A
+
+
+
+
+
+
+if __name__ == "__main__":
+        print __name__
+        A=[2, 5, 1, 8, 10]
+        print "Before:" + str(A)
+        B = INSERTION_SORT(A)
+        print "After: "+ str(B)
